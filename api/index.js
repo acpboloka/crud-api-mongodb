35cb0c7abb734d3e8d3a24d0d88427359fda91d7
acpboloka <115381281+acpboloka@users.noreply.github.com>
2025-12-12 09:00:02 -0300

    Update MongoDB connection string

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 // String de conexão MongoDB
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://admin:senha123@cluster0.xxxxx.mongodb.net/tarefas_db?retryWrites=true&w=majority&ssl=true';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://admin:<admin123>@cluster0.ltmmt6b.mongodb.net/?appName=Cluster0';
 
 let cachedClient = null;
 let cachedDb = null;
